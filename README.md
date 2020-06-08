4bd5beffed6fd2654505e9db88553049dedcf4e1
Terra_Ex <i8522-ghub@yahoo.com>
2020-06-08 23:53:53 +0100

    Basic implementation of shroud removal for both GFX modes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# CnC_Remastered_Collection
+# CnC_Remastered_Collection - Shroud / Fog of War Remover - Tiberium Dawn
+# Removes all shroud/fog of war from all maps. Useful for mappers/mission makers or anyone who doesn't want the fog of war.
--- TIBERIANDAWN/DLLInterface.cpp
+++ TIBERIANDAWN/DLLInterface.cpp
@@ -5452,8 +5452,8 @@ bool DLLExportClass::Get_Shroud_State(uint64 player_id, unsigned char *buffer_in
 
 			CNCShroudEntryStruct &shroud_entry = shroud->Entries[entry_index];
 
-			shroud_entry.IsVisible = cellptr->Is_Visible(PlayerPtr);
-			shroud_entry.IsMapped = cellptr->Is_Mapped(PlayerPtr);
+			shroud_entry.IsVisible = 
+			shroud_entry.IsMapped = true; // Terra_Ex - Shroud Remover
 			shroud_entry.IsJamming = false;
 			shroud_entry.ShadowIndex = -1;
 
